102106ca9d5f7cced7cc6fb0dbd522608827dd8c
Kylie Cuadra <45311838+kyliecuadra@users.noreply.github.com>
2023-01-17 00:56:12 +0800

    Add files via upload

--- assets/php/query.php
+++ assets/php/query.php
@@ -1,15 +1,13 @@
 <?php
 require("db_connection.php");
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $query = $_POST['query'];
 
 //Do your MySQL search here, something like:
-$result = mysqli_query($con, $query) or die (mysqli_error($con));
+$result = mysqli_query($con, $query);
 $i=1;
 $headerWritten = false;
 
 echo "<table style='text-align: center;'>";
-
 while ($query_result = mysqli_fetch_assoc($result)) {
     // Write header
     if(!$headerWritten) {
